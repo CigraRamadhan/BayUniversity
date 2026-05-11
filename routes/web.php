cb0d3417604bb3e0474c92e50be528c9fdd75398
Cigra Ramadhan <cigraramadhanrp@gmail.com>
2026-05-11 22:08:28 +0700

    Commit Kedua: Menambahkan Controller Dosen, Matkul, dan Jadwal, serta Blade pada views.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tes', function () {
+    return "Hello, World!";
+});
+
+Route::get('/dosen', function () {
+    return view('Dosen.index');
+});
+
